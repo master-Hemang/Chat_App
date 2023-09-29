e509a02434ed149c10501e0d365d0bfba1b86d1c
master-Hemang <101041055+master-Hemang@users.noreply.github.com>
2023-09-29 22:46:26 +0530

    Update db.js

--- Rest API/db.js
+++ Rest API/db.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb+srv://Grand_Hemang:hhhhvvvvbbbb@cluster0.u2uqsdn.mongodb.net/"
+
+const connectToMongo = async () => {
+    await mongoose.connect(mongoURI);
+    console.log("Connected to Mongo")
+};
+
+
+module.exports = connectToMongo;
 
